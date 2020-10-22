0481b2e982f4ee35573ab766dbaaee4830be9694
admin <admin@beebops-MacBook-Pro.local>
2020-10-22 19:09:36 +0300

    отображение маркеров на карте

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import PlacesList from "../places-list/places-list";
-import Map from "../map/map"
+import Map from "../map/map";
 
 const Main = (props) => {
 
@@ -106,13 +106,14 @@ const Main = (props) => {
             </section>
 
             <div className="cities__right-section">
-              <section className="cities__map map"></section>
+              <Map
+                offer={offer}
+              />
             </div>
           </div>
         </div>
       </main>
       <div>
-        <Map/>
       </div>
     </div>
   );
--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -1,49 +1,58 @@
 import React, {PureComponent} from "react";
 import leaflet from 'leaflet';
+import "leaflet/dist/leaflet.css";
 
 import PropTypes from "prop-types";
 
-const city = [52.38333, 4.9];
-const zoom = 12;
-const offerCords = [52.3709553943508, 4.89309666406198];
+const CITY = [52.38333, 4.9];
+const ZOOM = 12;
+
 
 class Map extends PureComponent {
   constructor(props) {
     super(props);
-
     this.mapContainer = React.createRef();
   }
 
   componentDidMount() {
+    const {offer} = this.props;
+
     const icon = leaflet.icon({
       iconUrl: `img/pin.svg`,
       iconSize: [30, 30]
     });
 
     const map = leaflet.map(this.mapContainer.current, {
-      center: city,
-      zoom: zoom,
+      center: CITY,
+      zoom: ZOOM,
       zoomControl: false,
       marker: true
     });
-    map.setView(city, zoom);
+    map.setView(CITY, ZOOM);
 
     leaflet.tileLayer(`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`, {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-  })
+      attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`
+    })
   .addTo(map);
 
-  leaflet.marker(offerCords, {icon}).addTo(map);
-
+    { offer.forEach((item) => (
+      leaflet.marker(item.coordinates, {icon}).addTo(map)
+    )); }
   }
 
   render() {
     return (
-        <section className="test">
-          <div ref={this.mapContainer} style={{height: `100%`}}></div>
-        </section>
-    )
+      <section className="cities__map map">
+        <div ref={this.mapContainer} style={{height: `100%`}}></div>
+      </section>
+    );
   }
 }
+
+Map.propTypes = {
+  offer: PropTypes.arrayOf(PropTypes.shape({
+    coordinates: PropTypes.array.isRequired,
+  })).isRequired,
+};
 
 export default Map;
